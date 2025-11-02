9ed27396db8c1b8cc8f907531db1fae7dd9b51ab
Sneha <sneha77nb@gmail.com>
2025-11-02 12:52:17 +0530

    Create Assignment1.q13.c

---
+++ Assignment1.q13.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+int i=1, n, sum=0;
+printf("Enter n: ");
+scanf("%d", &n);
+while(i <= n)
+  {
+      sum = sum + i;
+      i++;
+  }
+printf("Sum = %d", sum);
+return 0;
+}
